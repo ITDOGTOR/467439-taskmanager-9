51ecd1a56925e8d192d2a02357fe2348aaecc453
Роман <romkich16@gmail.com>
2019-08-18 21:40:35 +0300

    Создал вспомогательные функции

--- src/util.js
+++ src/util.js
@@ -1,8 +1,6 @@
-const TASK_COUNT = {
-  min: 16,
-  max: 50,
-};
+const getRandomInt = ({min, max}) => Math.floor(Math.floor(Math.random() * (max - min + 1) + min) / min) * min;
+const getRandomArrayElement = (array) => array[Math.floor((Math.random() * array.length) + 0)];
+const getRandomDate = () => Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000;
+const getRandomBoolean = () => Boolean(Math.random() > 0.5);
 
-const {min, max} = TASK_COUNT;
-
-export const getRandomInt = () => Math.floor(Math.floor(Math.random() * (max - min + 1) + min) / min) * min;
+export {getRandomInt, getRandomArrayElement, getRandomDate, getRandomBoolean};
