78064ac6403033e618d7fe27ad7067c6b544ad2e
Роман <romkich16@gmail.com>
2019-08-27 22:21:36 +0300

    Сделал функцию рандомного числа без кратности

--- src/util.js
+++ src/util.js
@@ -26,7 +26,7 @@ const unrenderElement = (element) => {
   }
 };
 
-const getRandomInt = ({min, max}) => Math.floor(Math.floor(Math.random() * (max - min + 1) + min) / min) * min;
+const getRandomInt = ({min, max}) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getRandomArrayElement = (array) => array[Math.floor((Math.random() * array.length) + 0)];
 const getRandomDate = () => Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000;
 const getRandomBoolean = () => Boolean(Math.random() > 0.5);
