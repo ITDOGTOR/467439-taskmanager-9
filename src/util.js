1cdaba191d3bc36a11efd7fd6ee3593c1fecd977
Роман <romkich16@gmail.com>
2019-08-27 18:47:14 +0300

    Создал функции создания и отрисовки DOM-элементов

--- src/util.js
+++ src/util.js
@@ -1,6 +1,34 @@
+const Position = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+const renderElement = (container, element, place = Position.BEFOREEND) => {
+  switch (place) {
+    case Position.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case Position.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+const unrenderElement = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
+
 const getRandomInt = ({min, max}) => Math.floor(Math.floor(Math.random() * (max - min + 1) + min) / min) * min;
 const getRandomArrayElement = (array) => array[Math.floor((Math.random() * array.length) + 0)];
 const getRandomDate = () => Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000;
 const getRandomBoolean = () => Boolean(Math.random() > 0.5);
 
-export {getRandomInt, getRandomArrayElement, getRandomDate, getRandomBoolean};
+export {createElement, renderElement, unrenderElement, getRandomInt, getRandomArrayElement, getRandomDate, getRandomBoolean};
