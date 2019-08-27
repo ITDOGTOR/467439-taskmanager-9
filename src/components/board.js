afeda1d2a98aa3ed4748f743cdd489c07f7cb4eb
Роман <romkich16@gmail.com>
2019-08-27 22:20:00 +0300

    Изменил расположение тегов в компоненте

--- src/components/board.js
+++ src/components/board.js
@@ -20,12 +20,12 @@ export default class Board {
 
   getTemplate() {
     return `<section class="board container">
+      <div class="board__filter-list">
+        <a href="#" class="board__filter">SORT BY DEFAULT</a>
+        <a href="#" class="board__filter">SORT BY DATE up</a>
+        <a href="#" class="board__filter">SORT BY DATE down</a>
+      </div>
       <div class="board__tasks">
-        <div class="board__filter-list">
-          <a href="#" class="board__filter">SORT BY DEFAULT</a>
-          <a href="#" class="board__filter">SORT BY DATE up</a>
-          <a href="#" class="board__filter">SORT BY DATE down</a>
-        </div>
       </div>
     </section>`;
   }
