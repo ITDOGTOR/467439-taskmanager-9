c5fdee1120ca0ed6d9c08b5b3e2a482240065021
Роман <romkich16@gmail.com>
2019-08-28 15:37:42 +0300

    Изменил и создал функции отрисовки компонентов + отрисовка компонента, если все задачи выполнены

--- src/main.js
+++ src/main.js
@@ -3,21 +3,26 @@ import Search from '../src/components/search.js';
 import FiltersContainer from '../src/components/filters-container.js';
 import Filter from '../src/components/filter.js';
 import Board from '../src/components/board.js';
+import BoardFilters from '../src/components/board-filters.js';
+import TasksContainer from '../src/components/tasks-container.js';
 import Task from '../src/components/task.js';
 import TaskEdit from '../src/components/task-edit.js';
 import LoadMoreButton from '../src/components/load-more-button.js';
+import NoTasks from '../src/components/no-tasks.js';
 
 import {taskMocks, filtersList} from '../src/data.js';
-import {renderElement} from './util.js';
+import {renderElement, Position} from '../src/util.js';
 import {TASK_COUNT} from '../src/constants.js';
 
 const {render} = TASK_COUNT;
+const {AFTERBEGIN} = Position;
 
 const copyTasks = taskMocks.slice();
 
 const renderTask = (taskMock) => {
   const task = new Task(taskMock);
   const taskEdit = new TaskEdit(taskMock);
+  const tasksContainer = document.querySelector(`.board__tasks`);
 
   const onEscKeyDown = (evt) => {
     if (evt.key === `Escape` || evt.key === `Esc`) {
@@ -47,40 +52,58 @@ const renderTask = (taskMock) => {
   renderElement(tasksContainer, task.getElement());
 };
 
-const renderTasks = (tasks) => tasks.splice(0, render).forEach((taskMock) => renderTask(taskMock));
+const renderFilter = (filter) => {
+  const filterElement = new Filter(filter);
+  const filtersContainer = document.querySelector(`.main__filter`);
 
-const renderFilters = (filters) => {
-  filters.forEach((filter) => {
-    const filterElement = new Filter(filter);
-    renderElement(filtersContainer, filterElement.getElement());
-  });
+  renderElement(filtersContainer, filterElement.getElement());
 };
+
+const renderTasks = (tasks) => tasks.splice(0, render).forEach((taskMock) => renderTask(taskMock));
+const renderFilters = (filters) => filters.forEach((filter) => renderFilter(filter));
 
 const mainContainer = document.querySelector(`.main`);
 const menuContainer = mainContainer.querySelector(`.main__control`);
-renderElement(menuContainer, new Menu().getElement());
-renderElement(mainContainer, new Search().getElement());
-renderElement(mainContainer, new FiltersContainer().getElement());
 
-const filtersContainer = mainContainer.querySelector(`.main__filter`);
-renderFilters(filtersList);
-renderElement(mainContainer, new Board().getElement());
+const renderMainComponents = () => {
+  renderElement(menuContainer, new Menu().getElement());
+  renderElement(mainContainer, new Search().getElement());
+  renderElement(mainContainer, new FiltersContainer().getElement());
+  renderFilters(filtersList);
+  renderElement(mainContainer, new Board().getElement());
 
-const boardContainer = mainContainer.querySelector(`.board`);
-const tasksContainer = boardContainer.querySelector(`.board__tasks`);
-renderElement(boardContainer, new LoadMoreButton().getElement());
-renderTasks(copyTasks);
+  const boardContainer = mainContainer.querySelector(`.board`);
+
+  if (!copyTasks.length) {
+    renderElement(boardContainer, new NoTasks().getElement());
+    return;
+  }
+
+  renderElement(boardContainer, new TasksContainer().getElement());
+  renderElement(boardContainer, new BoardFilters().getElement(), AFTERBEGIN);
+
+  if (copyTasks.length > render) {
+    renderElement(boardContainer, new LoadMoreButton().getElement());
+  }
+
+  renderTasks(copyTasks);
+};
+
+renderMainComponents();
 
 const onRenderMoreTask = () => {
   if (copyTasks.length) {
     renderTasks(copyTasks);
 
     if (!copyTasks.length) {
-      loadMoreElement.style.display = `none`;
-      loadMoreElement.removeEventListener(`click`, onRenderMoreTask);
+      loadMoreTasks.style.display = `none`;
+      loadMoreTasks.removeEventListener(`click`, onRenderMoreTask);
     }
   }
 };
 
-const loadMoreElement = boardContainer.querySelector(`.load-more`);
-loadMoreElement.addEventListener(`click`, onRenderMoreTask);
+const loadMoreTasks = document.querySelector(`.load-more`);
+
+if (loadMoreTasks) {
+  loadMoreTasks.addEventListener(`click`, onRenderMoreTask);
+}
